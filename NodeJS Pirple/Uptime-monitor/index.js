d21b9588365d598b720a8bc33a4a02d34ea280b5
Hasan Ahamed <officials.hasan@gmail.com>
2021-02-01 23:53:34 +0600

    2. Parsing Request paths

--- NodeJS Pirple/Uptime-monitor/index.js
+++ NodeJS Pirple/Uptime-monitor/index.js
@@ -4,11 +4,29 @@
 
 // Dependencies 
 const http = require('http');
-
+const url = require('url');
 
 // The server should respond to all request with a string
-const server = http.createServer((req,res)=>{
+const server = http.createServer(
+    (req,res)=>{ // a callback
+
+    // 1. Get the URL and Parse it!
+    const parsedURL = url.parse( req.url , true ); // when we create the server and tell it to listen, when we req comes in
+
+    // 2. Get the path from URL
+
+    const path = parsedURL.pathname; // untrimed path, path = hello
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+
+    // 3. Send the response
     res.end('hello world\n');
+    
+    // 4. log the request path
+    
+    console.log('Request recieved:' + trimmedPath);
+
+    // end callback
 })
 
 
