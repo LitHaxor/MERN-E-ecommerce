29508e7018d7d4c384ebfbf39f9376c473937987
Hasan Ahamed <officials.hasan@gmail.com>
2021-02-07 01:37:52 +0600

    created sample product in MongoDB

---
+++ MERN-ecommerce/Backend/models/ProductModel.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    name: {type:String, required: true, unique: true},
+    image: {type: String, required: true},
+    brand: { type:String, required: true},
+    category: { type:String, required: false},
+    description: { type:String, required: true},
+    price: { type:Number, required: true},
+    stock: { type:String, required: true},
+    rating: { type:String, required: true},
+    numReviews: { type:String, required: true},
+}, {timestamps: true}
+
+);
+
+const Product = new mongoose.model('Product', productSchema);
+
+export default Product;
--- MERN-ecommerce/Backend/server.js
+++ MERN-ecommerce/Backend/server.js
@@ -1,36 +1,37 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
+import productRouter from './routers/ProductRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
+
+//mongoDB driver
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/merne', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-app.use('/api/users', userRouter);
+// Home page
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
+// Routers
+app.use('/api/users', userRouter);
+
+app.use('/api/products', productRouter);
+
+
+// Middleware
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
+
+
+// Server Listener
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
